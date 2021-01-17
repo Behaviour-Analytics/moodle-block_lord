f510be1d29b5fd9d5d55c0a1bd42ddcae405a0f2
Ted Krahn <tkrahn1@athabasca.edu>
2021-01-17 11:13:13 -0700

    forgot to bump version after add amd to thirdpartylibs.xml

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_lord';
-$plugin->release   = '0.7.7';
-$plugin->version   = 2020110900;
+$plugin->release   = '0.7.8';
+$plugin->version   = 2021011700;
 $plugin->requires  = 2017111300;
